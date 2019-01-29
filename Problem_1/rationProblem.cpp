5c5afd76c55385e501e526645ed988aebaa51329
Filipe Falcão <filipefalcaosantos@gmail.com>
2019-01-29 08:42:56 -0300

    Code for Problem 1: Ration Problem (v2)

--- Problem_1/rationProblem.cpp
+++ Problem_1/rationProblem.cpp
@@ -8,16 +8,26 @@ int main() {
 	IloModel rationProblem(env, "Ration Problem");
 	IloCplex cplex(rationProblem);
 
+	// Statement Data:
+	double qtd_amgs[2] = {5, 1};
+	double qtd_re[2] = {2, 4};
+	double qtd[2] = {30000, 10000};
+	double amgs_price = 20, re_price = 30;
+	double meat_price = 4, cereal_price = 1;
+
 	// Decision Variables:
 	IloNumVar AMGS(env), RE(env);
 
 	// Restrictions:
-    rationProblem.add(1 * AMGS + 4 * RE <= 10000);
-    rationProblem.add(5 * AMGS + 2 * RE <= 30000);
-    rationProblem.add(AMGS >= 0 && RE >= 0);
+	rationProblem.add(AMGS >= 0 && RE >= 0);
+	for (int i = 0; i < 2; i++) {
+		rationProblem.add(qtd_amgs[i] * AMGS + qtd_re[i] * RE <= qtd[i]);
+	}
 
     // Objective Function:
-	rationProblem.add(IloMaximize(env, 11 * AMGS + 12 * RE));
+	rationProblem.add(IloMaximize(env, (amgs_price - qtd_amgs[0] * cereal_price - 
+		qtd_amgs[1] * meat_price) * AMGS + (re_price - qtd_re[0] * cereal_price - 
+		qtd_re[1] * meat_price) * RE));
 
 	// Get Solution:
     cplex.solve();
