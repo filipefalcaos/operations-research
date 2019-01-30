bd6cdd5df0066ef1a2dfc20307c7df451e837b29
Filipe Falcão <filipefalcaosantos@gmail.com>
2019-01-30 00:31:23 -0300

    Code for Problem 9: Traveling Salesman Problem

---
+++ Problem_9/travelingSalesman.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <ilcplex/ilocplex.h>
+using namespace std;
+
+// 2D Matrix of boolean variables
+typedef IloArray<IloBoolVarArray> BoolVarMatrix;
+
+int main() {
+    
+    IloEnv env;
+    IloModel travelingSalesman(env, "Traveling Salesman Problem");
+    IloCplex cplex(travelingSalesman);
+
+    // Statement Data:
+    int n;
+    scanf("%d", &n);
+
+    // Generate random distances between n cities
+    double distance[n][n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                distance[i][j] = 0;
+            } else {
+                distance[i][j] = static_cast <float> (rand()) / 
+                    (static_cast <float> (RAND_MAX / 100));
+            }
+        }
+    }
+
+    // Decision Variables:
+    IloNumVarArray s(env, n);
+    BoolVarMatrix vars(env, n);
+    
+    for (int i = 0; i < n; i++) {
+        s[i] = IloNumVar(env);
+        vars[i] = IloBoolVarArray(env, n);
+
+        for (int j = 0; j < n; j++) {
+            vars[i][j] = IloBoolVar(env);
+        }
+    }
+
+    // Restrictions:
+    // Unique output:
+    for (int i = 0; i < n; i++) {
+        IloExpr sum(env);
+
+        for (int j = 0; j < n; j++) {
+            if (i == j) {} else {
+                sum += vars[i][j];
+            }
+        }
+
+        travelingSalesman.add(sum == 1);
+    }
+
+    // Restrictions:
+    // Unique input:
+    for (int j = 0; j < n; j++) {
+        IloExpr sum2(env);
+
+        for (int i = 0; i < n; i++) {
+            if (i == j) {} else {
+                sum2 += vars[i][j];
+            }
+        }
+
+        travelingSalesman.add(sum2 == 1);
+    }
+
+    // Restrictions:
+    // Remove subcycles:
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            if (i == j) {} else {
+                IloExpr exp(env);
+                exp = s[i] - s[j] + (n - 1) * vars[i][j];
+                travelingSalesman.add(exp <= (n - 2));
+            }
+        }
+    }
+
+    // Objective Function:
+    IloExpr objective(env);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            objective += distance[i][j] * vars[i][j];
+        }
+    }
+    
+    travelingSalesman.add(IloMinimize(env, objective));
+
+    // Get Solution:
+    cplex.solve();
+    cout << "Objective Function: " << cplex.getObjValue() << endl;
+
+    // End
+    cplex.end();
+    travelingSalesman.end();
+    env.end();
+
+}
